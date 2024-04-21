a7aa5748335bc0a9b26374736b384b75e413240f
Aryan Tyagi <aryan.tyagi701378@gmail.com>
2024-04-21 23:45:17 +0530

    hardwork

--- experiment.js
+++ experiment.js
@@ -11,49 +11,17 @@ class Experiment {
     // this.runSteppers()
 
     // Hide Steppers
-    this.hideSteppers()
+    // this.hideSteppers()
     canvasSetup(this.canvasSel)
 
     // Clock
     // --------------------------------------------------
-    const clock = new Clock(this.canvasSel)
+    const animation = new Animation();
+    animation.draw();
     // const ms = document.timeline.currentTime
     // clock.draw(ms)
     // clock.draw(ms+25000)
-    const clockRafFn = (ts) => {
-      clock.startRace();
-      window.requestAnimationFrame(clockRafFn)
-    }
-     window.requestAnimationFrame(clockRafFn)
-    
+
   }
 
-  runSteppers() {
-
-    // Steppers
-    // --------------------------------------------------
-    const stepperOneRaf
-	  = window.requestAnimationFrame(stepperOne)
-
-    const stepperTwoRaf
-	  = window.requestAnimationFrame(stepperTwo)
-
-    const stepperThree = new StepperThree(
-      '#valueFromStepperThree', 15
-    )
-    const stepperThreeFn = (ts) => {
-      if (!stepperThree.isActive) stepperThree.start()
-      stepperThree.step(ts)
-      window.requestAnimationFrame(stepperThreeFn)
-    }
-    const stepperThreeRaf
-	  = window.requestAnimationFrame(stepperThreeFn)
-    // --------------------------------------------------
-    
-  }
-
-  hideSteppers() {
-    document.querySelector('#side-note')
-      .classList.add('hidden')
-  }
 }
